48f8a22d0235388c7f23061decb10344f74b7bdb
GG Gonzalez <gg.gonzalez@ymail.com>
2017-11-12 17:09:36 -0500

    Pieces go to the bottom open spot in board.

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ console.log("we have the meat$");
 // $gameBoard.append($column8);
 
 
+  let alternate = true;
+
 const generateBoard = () =>
 {
   const $gameBoard = $('<div>').addClass('gameBoard');
@@ -39,19 +41,73 @@ const generateBoard = () =>
   $('.container').append($gameBoard);
 }
 
+//make basic logic------------------------
+const playersTakeTurns = (event) => {
+
+  //EXPERIMENTING -----------------------
+  const $columns = $(event.currentTarget).parent();
+  const $test = $columns.children();
+  //
+  // const $circles = $(event.currentTarget);
+  //I want to add the circles as children of teh columns they're in, so when you click the column, the bottom-most non-'clicked' circle will change color
+  //line below is correct
+  for (let i = ($columns.children().length - 1); i >= 0; i--) {
+    // console.log('$columns.eq(i)', $columns.eq(i));
+    console.log('i', i);
+    // if($(event.currentTarget).attr('value')){
+    //run fxn below
+    //make loop around 'test', use line 86 syntax
+
+    // for (let i = $test[6]; i >= 0; i--){
+
+
+    if(!$test.eq(i).attr('value')){
+      if( alternate === true){
+        $test.eq(i).css('background-color', 'red').attr('value', 'clicked');
+        // console.log("Player1");
+        //Show text Player 1, it's your turn!
+        alternate = false;
+        return 0;
+      }else if(alternate === false){
+        $test.eq(i).css('background-color', 'blue').attr('value', 'clicked');
+        // console.log("Player2");
+        //Show text Player 2, it's your turn!
+        alternate = true;
+        return 0;
+      }
+
+
+      if($test.eq(i).attr('value')){
+        //move to i--
+        //run for (let i = $test[6]; i >= 0; i--){ again
+
+        //run fxn below
+
+      }
+    }
+    //correct below
+    console.log('event.target', $test[6]);
+
+    //   };// }; --------------can be disconnected here into 2 sep fxns
+    //Experimenting done----------------------------
+
+
+  }
+}
+
 
 $(() => {
 
-//make game board
-  let alternate = true;
+  //make game board
+
 
   // const $gameBoard = $('<div>').addClass('gameBoard');
   // const $circles = $('.circle');
   // const $columns = $('<.column>');
-generateBoard();
+  generateBoard();
 
 
-
+  $('.circle').on('click', playersTakeTurns);
   // make square grid - didn't work out----------------
   // for (let i = 0; i < 56; i++){
   //   let id = i + 1;
@@ -62,81 +118,34 @@ generateBoard();
   // };
 
   //make row function----------------------OBSOLETE
-// const assignColumns = (column) => {
-//   for (let i = 0; i < 7; i++){
-//     let num = i + 1;
-//     let id = 'row' + num;
-//     // let rowClass = 'row' + num;
-//     id.toString();
-//     let $circles = $('<div>').addClass('circle').attr('id', id);
-//     column.append($circles);
-//     };
-//   };
-//
-// //Calling column formation functions-------------------OBSOLETE
-// assignColumns($column1);
-// assignColumns($column2);
-// assignColumns($column3);
-// assignColumns($column4);
-// assignColumns($column5);
-// assignColumns($column6);
-// assignColumns($column7);
-// assignColumns($column8);
+  // const assignColumns = (column) => {
+  //   for (let i = 0; i < 7; i++){
+  //     let num = i + 1;
+  //     let id = 'row' + num;
+  //     // let rowClass = 'row' + num;
+  //     id.toString();
+  //     let $circles = $('<div>').addClass('circle').attr('id', id);
+  //     column.append($circles);
+  //     };
+  //   };
+  //
+  // //Calling column formation functions-------------------OBSOLETE
+  // assignColumns($column1);
+  // assignColumns($column2);
+  // assignColumns($column3);
+  // assignColumns($column4);
+  // assignColumns($column5);
+  // assignColumns($column6);
+  // assignColumns($column7);
+  // assignColumns($column8);
 
 
-//defining additional variables
+  //defining additional variables
 
 
-//make basic logic------------------------
-   const playersTakeTurns = (event) => {
 
-     //EXPERIMENTING -----------------------
-    const $columns = $(event.currentTarget).parent();
-    //
-    // const $circles = $(event.currentTarget);
-//I want to add the circles as children of teh columns they're in, so when you click the column, the bottom-most non-'clicked' circle will change color
-  //line below is correct
-       for (let i = ($columns.children().length - 1); i >= 0; i--) {
-         // console.log('$columns.eq(i)', $columns.eq(i));
-         console.log('i', i);
-         // if($(event.currentTarget).attr('value')){
-          //run fxn below
-          //make loop around 'test', use line 86 syntax
-          const $test = $(event.currentTarget).parent().children();
-            for (let i = $test[6]; i >= 0; i--){
-
-              if($test[i].attr('value')){
-                //move to i--
-                //run for (let i = $test[6]; i >= 0; i--){ again
-
-               //run fxn below
-
-          }
-        }
-             //correct below
-             console.log('event.target', $test[6]);
-
-           //   };// }; --------------can be disconnected here into 2 sep fxns
-       //Experimenting done----------------------------
-
-     if(!$(event.currentTarget).attr('value')){
-       if( alternate === true){
-         $(event.currentTarget).css('background-color', 'red').attr('value', 'clicked');
-         // console.log("Player1");
-         //Show text Player 1, it's your turn!
-         alternate = false;
-       }else if(alternate === false){
-         $(event.currentTarget).css('background-color', 'blue').attr('value', 'clicked');
-         // console.log("Player2");
-         //Show text Player 2, it's your turn!
-         alternate = true;
-     }else{
-         return false;
-           };
-          };
-        };
-      };
-    // };
+});
+// };
 
 
 
@@ -145,17 +154,17 @@ generateBoard();
 
 //Calling take turns function
 $('.circle').on('click', playersTakeTurns);
+//
+// //testing with console.logsssss --------------------------
+// $('#column1').on('click', () =>{console.log('column1')});
+// $('#column2').on('click', () =>{console.log('column2')});
+// $('#column3').on('click', () =>{console.log('column3')});
+// $('#column4').on('click', () =>{console.log('column4')});
+// $('#column5').on('click', () =>{console.log('column5')});
+// $('#column6').on('click', () =>{console.log('column6')});
+// $('#column7').on('click', () =>{console.log('column7')});
 
-//testing with console.logsssss --------------------------
-$column1.on('click', () =>{console.log('column1')});
-$column2.on('click', () =>{console.log('column2')});
-$column3.on('click', () =>{console.log('column3')});
-$column4.on('click', () =>{console.log('column4')});
-$column5.on('click', () =>{console.log('column5')});
-$column6.on('click', () =>{console.log('column6')});
-$column7.on('click', () =>{console.log('column7')});
-
-$('.circle').on('click', () =>{console.log($(event.currentTarget).attr('id'))});
+// $('.circle').on('click', () =>{console.log($(event.currentTarget).attr('id'))});
 
 
 //Win cases
@@ -175,5 +184,5 @@ $('.circle').on('click', () =>{console.log($(event.currentTarget).attr('id'))});
 
 
 
-});
+// });
 // });
